1e1956e25d14c4d0ab70b687744ee403848d342e
Pavel Volkov <pvolkov@dataart.com>
2018-12-08 22:40:49 +0300

    Added logging to RequestHelper

--- MoexPortfolioSimulator/Helpers/RequestsHelper.cs
+++ MoexPortfolioSimulator/Helpers/RequestsHelper.cs
@@ -1,14 +1,18 @@
 using System.Net.Http;
 using System.Threading.Tasks;
+using log4net;
 
 namespace MoexPortfolioSimulator.Helpers
 {
     public class RequestsHelper
     {
+        private static ILog logger => LogManager.GetLogger(typeof(RequestsHelper));
+
         private static readonly HttpClient Client = new HttpClient();
         
         public static async Task<string> SendGetRequest(string url)
         {
+            logger.Debug("Send Get request: " + url);
             var msg = new HttpRequestMessage(HttpMethod.Get, url);
             var response = await Client.SendAsync(msg);
             response.EnsureSuccessStatusCode();
