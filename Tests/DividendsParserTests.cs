20a25c72bf419fd075bac5c61abe3d9d018a25a1
Pavel Volkov <pvolkov@dataart.com>
2018-12-08 13:51:37 +0300

    Added Tests for Dohod provider

---
+++ Tests/DividendsParserTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Threading.Tasks;
+using MoexPortfolioSimulator.Data;
+using MoexPortfolioSimulator.Data.Providers;
+using NUnit.Framework;
+
+namespace Tests
+{
+    [TestFixture]
+    public class Tests
+    {
+        [Test]
+        public async Task VerifyDivsCount()
+        {
+            var divs = await Dohod.GetDividendsBySymbol("sber");
+            Assert.True(divs != null && divs.Count >= 16);
+        }
+        
+        [Test]
+        public async Task VerifyDivsTableParser()
+        {
+            var expectedDiv = new Dividend(DateTime.Parse("04.06.2018"), new decimal(0.0034535));
+            var divs = await Dohod.GetDividendsBySymbol("vtbr");
+            Assert.True(divs != null && divs.Count > 0);
+            Assert.Contains(expectedDiv, divs);
+        }
+    }
+}
